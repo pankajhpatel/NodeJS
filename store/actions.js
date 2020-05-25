90f3f20eb6effad5ae2a7f0641d7a09887335694
Pankaj <61523795+pankajhpatel@users.noreply.github.com>
2020-05-25 15:50:17 -0400

    Adding ADD and Delete Square of Counter to list Box
    On Clicking the List Box it clear the list from displaying

---
+++ store/actions.js
@@ -0,0 +1,6 @@
+export const INCR='INCR';
+export const DECR='DECR';
+export const ADD='ADD';
+export const SUB='SUB';
+export const ADD_SQUARE='ADD_SQUARE';
+export const DELETE_SQUARE='DELETE_SQURE'
--- store/reducer.js
+++ store/reducer.js
@@ -1,31 +1,51 @@
+import * as actionTypes from './actions'
 const initState = {
-    counter: 104
+    counter: 1,results:[]
 }
 const reducer = (state = initState, action) => {
-    if (action.type === 'INC') {
+    if (action.type === actionTypes.INCR) {
         return {
             ...state,
             counter: state.counter + 1
         }
     }
-    if (action.type === 'DEC') {
+    if (action.type === actionTypes.DECR) {
         return {
             ...state,
             counter: state.counter - 1
         }
     }
-    if (action.type === 'ADD') {
+    if (action.type === actionTypes.ADD) {
         return {
             ...state,
             counter: state.counter + action.value
         }
     }
-    if (action.type === 'SUB') {
+    if (action.type === actionTypes.SUB ) {
         return {
             ...state,
             counter: state.counter - action.value
         }
     }
+    if (action.type === actionTypes.ADD_SQUARE){
+        return{
+            ...state,
+            results:state.results.concat(state.counter*state.counter)
+        }
+    }
+    if (action.type === actionTypes.DELETE_SQUARE){
+          /*  console.log("DELETE ON LIST"+action.value);  
+            let memps = state.results;
+            memps.splice(action.value,1);
+            state.results=memps;
+            console.log(state.results);*/
+        return{
+            ...state,
+            results:state.results.filter((_,i)=>i!==action.value)
+        }
+    }
+
+
     return state;
 }
 export default reducer;
