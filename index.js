815c4967f50b9f4894cdbafd59b121820fe054d8
Pankaj <61523795+pankajhpatel@users.noreply.github.com>
2020-06-02 14:10:40 -0400

    Adding custom Middleware and enhancer to debug Redux Dev Tool in Chrome

--- index.js
+++ index.js
@@ -1,20 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import {createStore, combineReducers, applyMiddleware} from 'redux';
+import {createStore, combineReducers, compose, applyMiddleware} from 'redux';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import counterReducer from './store/reducers/counter'
 import squareReducer from './store/reducers/square'
+import factorialReducer from './store/reducers/factorial'
 import {Provider} from 'react-redux';
 import thunk from 'redux-thunk';
 
 const rootReducer = combineReducers({
   ctr:counterReducer,
-  msqr:squareReducer
+  msqr:squareReducer,
+  mfact:factorialReducer
 }) 
 
-const store=createStore(rootReducer,applyMiddleware(thunk));
+const logger = store => {
+  return next => {
+    return action => {
+      console.log( '{Middleware] dispatching ', action);
+      const result = next(action);
+      console.log('[Middleware] next state',store.getState());
+      return result;
+    }
+  }
+}
+
+//const enhancers = compose(
+//  window.devToolsExtension ? window.devToolExtension(): f => f
+//);
+
+const enhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store=createStore(rootReducer,  enhancers(applyMiddleware(thunk,logger)));
+
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}><App /> </Provider>
