035783396e2cfb450589a88933feecc51dc49723
Pankaj <61523795+pankajhpatel@users.noreply.github.com>
2020-06-02 14:19:56 -0400

    Adding Factorial Functionality and CSS for listing

--- containers/MainPage.js
+++ containers/MainPage.js
@@ -3,6 +3,7 @@ import DisplayCounter from  '../components/DisplayCounter/DisplayCounter'
 import ChangeCounter from '../components/ChangeCounter/ChangeCounter'
 import {connect} from 'react-redux';
 import * as actionCreators from '../store/actions';
+import './ulstyle.css';
 
 class MainPage extends Component{
     state={
@@ -36,13 +37,26 @@ class MainPage extends Component{
                <ChangeCounter click={this.props.onSubCounter}caption="SUB 5"/>
                <hr></hr>
                <button onClick={()=>this.props.onAddSquare(this.props.ctr)}>Square and Store</button>
-               <ul>
+               &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+               <button onClick={()=>this.props.onAddFactorial(this.props.ctr)}>Factorial and Store</button>
+               <div className="ulsqrdiv">
+               <ul className="ulsquare" >
                    {
                        this.props.storedResults.map((sqr,index)=>
                         (<li key={index}onClick={()=>this.props.onDeleteSquare(index)}>{sqr}</li>)
                         )
                    }
                </ul>
+               </div>
+               <div className="ulfactdiv">
+               <ul className="ulfactorial">
+                   {
+                       this.props.storedFactResults.map((fact,index)=>
+                        (<li key={index}onClick={()=>this.props.onDeleteFactorial(index)}>{fact}</li>)
+                        )
+                   }
+               </ul>
+               </div>
             </div>
         )
     }
@@ -51,9 +65,9 @@ class MainPage extends Component{
 const mapStateToProps = state=>{
     return{
         ctr:state.ctr.counter, 
-        storedResults:state.msqr.results
+        storedResults:state.msqr.results,
+        storedFactResults:state.mfact.factresults
         }
-
 }
 const mapDispatchToProps = dispatch =>{
     return{
@@ -61,8 +75,11 @@ const mapDispatchToProps = dispatch =>{
         onDecrementCounter:()=>dispatch(actionCreators.decrement()),
         onAddCounter:()=>dispatch(actionCreators.addition()),
         onSubCounter:()=>dispatch(actionCreators.subtraction()),
-        onAddSquare:(counter)=>dispatch(actionCreators.addsquare(counter)),
-        onDeleteSquare:(i)=>dispatch(actionCreators.deletesquare(i))
+        onAddSquare:(counter)=>dispatch(actionCreators.addSquare(counter)),
+        onDeleteSquare:(i)=>dispatch(actionCreators.deletesquare(i)),
+        onAddFactorial:(counter)=>dispatch(actionCreators.addFactorial(counter)),
+        onDeleteFactorial:(i)=>dispatch(actionCreators.deleteFactorial(i))
+       
     }
 }
 
